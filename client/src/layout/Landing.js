985693d6e68e9d203aed048a29c64beb72bee5d6
Doowankim <rlaendhks11@gmail.com>
2020-04-03 17:45:03 +0900

    user login route & model, passport, validation & Add client

---
+++ client/src/layout/Landing.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import styled from "styled-components";
+
+const Back = styled.div`
+  margin-top: 50px;
+  margin-bottom: 50px;
+  margin-right: 40px;
+  margin-left: 40px;
+`;
+
+class Landing extends Component {
+    render() {
+        return (
+            <Back>
+                Landing
+            </Back>
+        );
+    }
+}
+
+export default Landing;
